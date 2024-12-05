4d6f500a81604a2cbb71f411fb605265af1e4f1a
pblgllgs <pbl.gllgs@gmail.com>
2024-12-05 12:30:21 -0300

    add bean with cors configuration

---
+++ src/main/java/com/pblgllgs/gatewayservice/config/AppConfig.java
@@ -0,0 +1,30 @@
+package com.pblgllgs.gatewayservice.config;
+/*
+ *
+ * @author pblgl
+ * Created on 04-12-2024
+ *
+ */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
